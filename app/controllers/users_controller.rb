8bbc885026a29e8b6e2ad5ee6a6ffc16b944ee89
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-09-19 14:51:19 +0000

    [Add]users_index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:show]
+  
   def new
   end
 
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
 
   def show
-    if user_signed_in?
-      @user = User.find(params[:id])
-      @user_books = @user.books.all
-      @book = Book.new
-    end
+    @user = User.find(params[:id])
+    @user_books = @user.books.all
+    @book = Book.new
   end
 
   def edit
+  end
+  
+  private
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to root_path
+    end
   end
 end
